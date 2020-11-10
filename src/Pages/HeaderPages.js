d5eaf06108d5228e779683c9e87cefd2642215c4
TimmerMans <flynnestwood@gmail.com>
2020-11-10 18:32:36 +0200

    Add routing

---
+++ src/Pages/HeaderPages.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+export class Home extends Component {
+	render() {
+		return (
+			<div>
+				Home!
+			</div>
+		)
+	}
+}
+
+export class About extends Component {
+	render() {
+		return (
+			<div>About!</div>
+		);
+	}
+}
+
+export class Contacts extends Component {
+	render() {
+		return (
+			<div>Contacts!</div>
+		);
+	}
+}
+
+export class Blog extends Component {
+	render() {
+		return (
+			<div>Blog!</div>
+		);
+	}
+}
